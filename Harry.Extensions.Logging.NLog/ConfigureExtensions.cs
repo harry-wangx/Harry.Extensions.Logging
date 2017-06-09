de7340f53e132e369a3aa79f4eb694807cd7b3fe
Harry <harry_w@foxmail.com>
2017-06-09 17:02:55 +0800

    实现基本功能

---
+++ Harry.Extensions.Logging.NLog/ConfigureExtensions.cs
@@ -0,0 +1,96 @@
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
+
+namespace Harry.Extensions.Logging.NLog
+{
+    /// <summary>
+    /// Helpers for .NET Core
+    /// </summary>
+    public static class ConfigureExtensions
+    {
+        /// <summary>
+        /// Enable NLog as logging provider in .NET Core.
+        /// </summary>
+        /// <param name="factory"></param>
+        /// <returns>ILoggerFactory for chaining</returns>
+        public static ILoggerFactory AddNLog(this ILoggerFactory factory)
+        {
+            return AddNLog(factory, null);
+        }
+
+        /// <summary>
+        /// Enable NLog as logging provider in .NET Core.
+        /// </summary>
+        /// <param name="factory"></param>
+        /// <param name="options">NLog options</param>
+        /// <returns>ILoggerFactory for chaining</returns>
+        public static ILoggerFactory AddNLog(this ILoggerFactory factory, NLogProviderOptions options)
+        {
+            //ignore this
+            LogManager.AddHiddenAssembly(Assembly.Load(new AssemblyName("Microsoft.Extensions.Logging")));
+            LogManager.AddHiddenAssembly(Assembly.Load(new AssemblyName("Microsoft.Extensions.Logging.Abstractions")));
+
+            try
+            {
+                //try the Filter ext
+                var filterAssembly = Assembly.Load(new AssemblyName("Microsoft.Extensions.Logging.Filter"));
+                LogManager.AddHiddenAssembly(filterAssembly);
+            }
+            catch (Exception)
+            {
+                //ignore
+            }
+          
+            LogManager.AddHiddenAssembly(typeof(ConfigureExtensions).GetTypeInfo().Assembly);
+
+            using (var provider = new NLogLoggerProvider(options))
+            {
+                factory.AddProvider(provider);
+            }
+            return factory;
+        }
+
+        /// <summary>
+        /// Apply NLog configuration from XML config.
+        /// </summary>
+        /// <param name="env"></param>
+        /// <param name="configFileRelativePath">relative path to NLog configuration file.</param>
+          /// <returns>Current configuration for chaining.</returns>
+        public static LoggingConfiguration ConfigureNLog(this ILoggerFactory env, string configFileRelativePath)
+        {
+            var rootPath = System.AppContext.BaseDirectory;
+
+            var fileName = Path.Combine(rootPath, configFileRelativePath);
+            return ConfigureNLog(fileName);
+        }
+
+      /// <summary>
+       /// Apply NLog configuration from config object.
+       /// </summary>
+       /// <param name="env"></param>
+       /// <param name="config">New NLog config.</param>
+       /// <returns>Current configuration for chaining.</returns>
+       public static LoggingConfiguration ConfigureNLog(this ILoggerFactory env, LoggingConfiguration config)
+       {
+           LogManager.Configuration = config;
+
+           return config;
+       }
+
+        /// <summary>
+        /// Apply NLog configuration from XML config.
+        /// </summary>
+        /// <param name="fileName">absolute path  NLog configuration file.</param>
+          /// <returns>Current configuration for chaining.</returns>
+        private static LoggingConfiguration ConfigureNLog(string fileName)
+        {
+            var config = new XmlLoggingConfiguration(fileName, true);
+            LogManager.Configuration = config;
+            return config;
+        }
+    }
+}
